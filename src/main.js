1f633292c22f7f0d59e672d76b3ec4f62cd0701a
Itamar Ostricher <itamarost@gmail.com>
2017-11-27 12:48:39 +0200

    Add firebase & firebase-ui, bang head against wall

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import 'firebase'
+import 'firebaseui'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App'
 import router from './router'
 import store from './store'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
