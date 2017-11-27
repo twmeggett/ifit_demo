504b635f21903eca62c06470111ec976879fcb3f
Itamar Ostricher <itamarost@gmail.com>
2017-11-27 10:08:17 +0200

    Change navbar variant

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="md" type="dark" variant="info">
+    <b-navbar toggleable="md" type="dark" variant="danger">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-navbar-brand :to="{name: 'HelloWorld'}">Vue 2</b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
