0b26e03e1a57f72fe1e44b4081a69ce0171c2b65
Tharin Meggett II <twmeggett@gmail.com>
2018-12-23 18:03:28 -0500

    removed unused components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
 import Payments from '@/components/Payments'
 
 Vue.use(Router)
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Payments',
       component: Payments
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
     }
   ]
 })
