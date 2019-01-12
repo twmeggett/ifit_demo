8b5e1a4784d59df5473993a8408bf8ffa9777a6e
Tharin Meggett II <twmeggett@gmail.com>
2019-01-11 22:47:43 -0500

    fixed prod for ifit_demo

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -25,7 +25,7 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    publicPath: "https://twmeggett.github.io/pint_layout/",
+    publicPath: "https://twmeggett.github.io/ifit_demo/",
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
